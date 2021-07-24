b2b3ff983ee2e83d03c418d646e69181a5a30885
wanxianq <wanxianquek@gmail.com>
2021-07-24 12:10:54 +0800

    Edited styling for Listings

--- src/components/ListingCard.js
+++ src/components/ListingCard.js
@@ -7,7 +7,7 @@ import {
 
 function ListingCard(props) {
     return (
-        <Col>
+        <Col style={{paddingTop:20, paddingBottom:20}}>
             <Card style={{ width: "18rem" }}>
                 <Card.Img variant="top" src={props.image} />
                 <Card.Body>
